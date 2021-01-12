f870a6546cd24391063a335435a81ce0a1d06348
Eduardo <ex.romeroma@gmail.com>
2021-01-12 19:11:42 +0100

    #

--- Javascript/PersistentBugger.js
+++ Javascript/PersistentBugger.js
@@ -2,3 +2,16 @@
 // and returns its multiplicative persistence, 
 // which is the number of times you must multiply the digits
 //  in num until you reach a single digit.
+
+function persistence(num) {
+    let steps = 0;
+    let integer = num.toString();
+    while (integer.length > 1) {
+        steps++
+        let mapped = integer.split('').reduce((acc, curr) => parseInt(acc) * parseInt(curr));
+        integer = mapped.toString();
+    }
+    return steps;
+}
+
+persistence(39) // Output '3';
