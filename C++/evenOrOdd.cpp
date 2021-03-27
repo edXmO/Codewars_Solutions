60f3a9e89afd7f2dc895404748ab134d1b86cd92
Eduardo <ex.romeroma@gmail.com>
2021-03-27 09:59:22 +0100

    first cpp practice

---
+++ C++/evenOrOdd.cpp
@@ -0,0 +1,4 @@
+std::string even_or_odd(int number) 
+{
+  return numer % 2 == 0 ? "Even" : "Odd";
+}
---
+++ C++/paperwork.cpp
@@ -0,0 +1,6 @@
+int paperwork(int n, int m)
+{
+  if(n < 0 || m < 0) return 0;
+  int result = n * m;
+  return result;
+}
